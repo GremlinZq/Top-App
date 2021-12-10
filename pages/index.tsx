94224d9323d4610d321083c2acf8e384f4fc6572
GremlinZq <gremzyx@bk.ru>
2021-12-10 18:22:31 +0300

    create next app

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const HomePage = () => {
+    return (
+        <>
+            <Head>
+                <title>Top App</title>
+                <meta name="description" content="Top App" />
+            </Head>
+
+            <div>next start</div>
+        </>
+    )
+}
+
+export default HomePage;
