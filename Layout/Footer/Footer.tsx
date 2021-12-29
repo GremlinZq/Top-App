d851ca8ff74d86f73f19e779782812161ab522d1
GremlinZq <gremzyx@bk.ru>
2021-12-29 03:18:19 +0300

    create footer layout

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -2,7 +2,17 @@
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <div className={styles.footer}>footer</div>
+        <div className={styles.footer}>
+            <div className={styles.left}>
+                <span>OwlTop © 2020 - {currentYear} Все права защищены</span>
+            </div>
+            <div className={styles.right}>
+                <a href='#'>Пользовательское соглашение</a>
+                <a href='#'>Политика конфиденциальности</a>
+            </div>
+        </div>
     )
 }
