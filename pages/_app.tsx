dca53995433f3902f7b4a520b9997b993987fe6b
GremlinZq <gremzyx@bk.ru>
2021-12-30 06:14:34 +0300

    add redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 // Core
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { store } from '../redux';
 import {Layout} from "../Layout";
 
 // Styles
@@ -12,10 +14,12 @@ const MyApp = ({Component, pageProps}: AppProps) => (
             <title>Top App</title>
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@300;400;500;700&display=swap" rel="stylesheet"/>
         </Head>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </Provider>
     </>
-)
+);
 
 export default  MyApp;
